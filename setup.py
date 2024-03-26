a877233780a2d82c6c0073532be4f181b3cf0262
Nilesh Patel <62102565+nitinilesh@users.noreply.github.com>
2024-03-27 03:07:46 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+     name = 'wellnet',
+     version ='1.0.0',
+     author = 'Tenacity',
+     packages= find_packages(),
+     install_requires =[]
+)
