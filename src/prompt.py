77709b052c98a64a1a7cac5d2286434368e8b47f
Nilesh Patel <62102565+nitinilesh@users.noreply.github.com>
2024-03-31 00:09:43 +0530

    changes updated

--- src/prompt.py
+++ src/prompt.py
@@ -1,10 +1,10 @@
-prompt_template="""
-Use the following pieces of information to answer the user's question.
-If you don't know the answer, just say that you don't know, don't try to make up an answer.
+prompt_template = """
+Utilize the provided details to respond to the user's inquiry.
+If you lack the answer, simply state that you're unaware, refrain from inventing a response.
 
 Context: {context}
-Question: {question}
+Queston: {question}
 
-Only return the helpful answer below and nothing else.
-Helpful answer:
+Kindly provide solely the useful response below.
+Useful response:
 """
